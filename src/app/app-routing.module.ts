32407c413a9c2bcfbe497133c6c697ceab391b6f
sbore669 <107039494+sbore669@users.noreply.github.com>
2023-01-15 19:03:18 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+   // loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+   loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
+
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  {
+    path: 'connexion',
+    loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
+  },
+  {
+    path: 'inscription',
+    loadChildren: () => import('./inscription/inscription.module').then( m => m.InscriptionPageModule)
   }
 ];
 @NgModule({
